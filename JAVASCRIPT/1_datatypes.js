33374152f4f30964b2c39e0c5b148cd65d8883f4
VENKATA SAI SIDDHARDHA KORADA <kvsaisiddhardha2117@gmail.com>
2026-07-07 14:18:09 +0000

    Data Types included

---
+++ JAVASCRIPT/1_datatypes.js
@@ -0,0 +1,13 @@
+// can be changes
+let name = "siddhu"
+let ans = false
+
+// var can also be used but not considered because of scope difference
+var fullName = "Siddhardha"
+
+// const cannot be changed once declared
+const pi = 3.14
+
+console.log(name, ans)
+console.log(fullName)
+console.log(pi)
